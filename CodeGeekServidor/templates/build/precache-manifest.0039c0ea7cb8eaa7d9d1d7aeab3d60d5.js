0f5aef6ad2ca4e58845583ba21710dd4d1344bad
Benja789 <molina.benjamin70@gmail.com>
2021-10-05 08:46:41 -0600

    G8T-41 Creacion del build de la vista

---
+++ CodeGeekServidor/templates/build/precache-manifest.0039c0ea7cb8eaa7d9d1d7aeab3d60d5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "efb9b44e4ab0c487de3b",
+    "url": "/static/css/main.597279d5.chunk.css"
+  },
+  {
+    "revision": "efb9b44e4ab0c487de3b",
+    "url": "/static/js/main.7ea8348e.chunk.js"
+  },
+  {
+    "revision": "d6111a72daa1a50f992c",
+    "url": "/static/js/runtime~main.dc7572c9.js"
+  },
+  {
+    "revision": "347d64756b886022f8a6",
+    "url": "/static/css/2.573b0d66.chunk.css"
+  },
+  {
+    "revision": "347d64756b886022f8a6",
+    "url": "/static/js/2.e2b94776.chunk.js"
+  },
+  {
+    "revision": "f44e58a0e1ac4d2822ce",
+    "url": "/static/js/3.0a5f563b.chunk.js"
+  },
+  {
+    "revision": "e356eb481b8cd8f93c07a1d524670ed8",
+    "url": "/static/media/logo.e356eb48.png"
+  },
+  {
+    "revision": "63ac3fde4f7863690f79029c870fc822",
+    "url": "/static/media/AcademicaFrontal.63ac3fde.jpg"
+  },
+  {
+    "revision": "6dbe4d47a7460524cd35be4bb4ff656c",
+    "url": "/static/media/AcademicaLateral.6dbe4d47.jpg"
+  },
+  {
+    "revision": "08601732b09526474d087dee5e194f7b",
+    "url": "/static/media/EdificioB.08601732.jpg"
+  },
+  {
+    "revision": "38efadb05acf53ba1a7e5e5b9e057822",
+    "url": "/static/media/AcademicaAtardecer.38efadb0.jpeg"
+  },
+  {
+    "revision": "e836a1bfed827f097826e02d7e57c26f",
+    "url": "/index.html"
+  }
+];
