beff0f5db4fedc4a876e00fcc8a28ff10841b653
Ronaldo Martinez <irmv_0311@hotmail.com>
2021-11-08 01:01:48 -0600

    build deploy -4

--- CodeGeekServidor/CodeGeekServidor/urls.py
+++ CodeGeekServidor/CodeGeekServidor/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('materias/', include('GestionMaterias.urls')),
     
     path('', include('VistasGenerales.urls')), #Cambiar despues a metodo
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
