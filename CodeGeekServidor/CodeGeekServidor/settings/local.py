57e51415bb0ec9e805960ce0b97cdf3d6f109298
Ronaldo Martinez <irmv_0311@hotmail.com>
2021-11-28 00:14:26 -0600

    Cambio de db a postgress y reinicio de migraciones

--- CodeGeekServidor/CodeGeekServidor/settings/local.py
+++ CodeGeekServidor/CodeGeekServidor/settings/local.py
@@ -6,6 +6,40 @@ DEBUG =True
 ALLOWED_HOSTS=[]
 
 DATABASES = {
+     'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'too',
+
+        'USER': 'too',
+
+        'PASSWORD': 'admin',
+
+        'HOST': 'proyectos.czoy1vdwgmhj.us-east-1.rds.amazonaws.com',
+
+        'PORT': '5432',
+
+    }
+}
+'''
+ 'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'too',
+
+        'USER': 'too',
+
+        'PASSWORD': 'admin',
+
+        'HOST': 'proyectos.czoy1vdwgmhj.us-east-1.rds.amazonaws.com',
+
+        'PORT': '5432',
+
+    }
+'''
+'''
     'default': {
         'ENGINE': 'django.db.backends.oracle',
         'NAME': 'geek',
@@ -14,7 +48,7 @@ DATABASES = {
         'HOST': 'geek3.czoy1vdwgmhj.us-east-1.rds.amazonaws.com',
         'PORT': '1521',
     }
-}
+'''
 # sqlplus /nolog
 # CONNECT test3/admin@//geek3.czoy1vdwgmhj.us-east-1.rds.amazonaws.com:1521/geek
 
